6f000b775ed6f6631986dc9a06d484fb470879e1
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2022-09-04 18:31:12 +0900

    修正：SECURITY_KEYの保護

--- sample/settings.py
+++ sample/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .local_settings import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&^$5bwsddf*!j7nbw75yzpm+2b1igzjnt^b0^sy_fiv!heq6n0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
